0805e6adac76f0f3b412d3c5f5f522df718d787b
Kirill Somov <gornomirov661@gmail.com>
2025-04-17 16:58:18 +0300

    Initial Commit

---
+++ ShibaLoader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShibaLoader
+//
+//  Created by Сомов Кирилл on 17.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
